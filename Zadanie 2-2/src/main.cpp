46d923585149e5dab90ae1afc907f6eff9df2c06
adam.dudkiewicz@student.wat.edu.pl <adam.dudkiewicz@student.wat.edu.pl>
2020-11-23 16:55:44 +0100

    Wykonano zadanie 2.2.2.

--- Zadanie 2-2/src/main.cpp
+++ Zadanie 2-2/src/main.cpp
@@ -1,13 +1,38 @@
-#include <Arduino.h>
+#define LED_RED 10
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+// Sposób kodowania
+#define M_KROPKA_T 200
+#define M_KRESKA_T 3 * M_KROPKA_T
+#define M_PAUZA_T 3 * M_KROPKA_T
+#define M_SPACJA_T 7 * M_KROPKA_T
+
+void setup()
+{
+  pinMode(LED_RED, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(LED_BUILTIN,LOW);
+void loop()
+{
+  for (int i=0;i < 3; i++)
+  {
+  digitalWrite(LED_RED, HIGH);
+  delay(M_KROPKA_T);
+  digitalWrite(LED_RED, LOW);
+  delay(M_PAUZA_T);
+  }
+  for (int i=0; i < 3; i++)
+  {
+  digitalWrite(LED_RED, HIGH);
+  delay(M_KRESKA_T);
+  digitalWrite(LED_RED, LOW);
+  delay(M_PAUZA_T);
+  }
+  for (int i = 0; i < 3; i++)
+  {
+   digitalWrite(LED_RED, HIGH);
+  delay(M_KROPKA_T);
+  digitalWrite(LED_RED, LOW);
+  delay(M_PAUZA_T);
+  }
+  delay(M_SPACJA_T);
 }
